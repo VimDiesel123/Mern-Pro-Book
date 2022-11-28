0935bda8eb07c0dea970cc83d7cde454a7686bf7
david <dsmith@securescan.com>
2022-11-28 15:58:53 -0500

    Construct issue rows programatically from array

--- src/App.jsx
+++ src/App.jsx
@@ -9,33 +9,40 @@ class IssueFilter extends React.Component {
 
 class IssueTable extends React.Component {
     render() {
-        const borderedStyle = { border: "1px solid silver", padding: 6 };
+        const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
         return (
             <table style={{ borderCollapse: "collapse" }}>
                 <thead>
                     <tr>
-                        <th style={borderedStyle}>Id</th>
-                        <th style={borderedStyle}>Title</th>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>Owner</th>
+                        <th>Created</th>
+                        <th>Effort</th>
+                        <th>Completion</th>
+                        <th>Title</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <IssueRow issue_id={1}
-                        issue_title="Error in console when clicking Add" />
-                    <IssueRow issue_id={2}
-                        issue_title="Missing bottom border on panel" />
+                    {issueRows}
                 </tbody>
-            </table>
+            </table >
         )
     }
 }
 
 class IssueRow extends React.Component {
     render() {
-        const borderedStyle = { border: "1px solid silver", padding: 4 };
+        const issue = this.props.issue;
         return (
             <tr>
-                <td style={borderedStyle}>{this.props.issue_id}</td>
-                <td style={borderedStyle}>{this.props.issue_title}</td>
+                <td>{issue.id}</td>
+                <td>{issue.status}</td>
+                <td>{issue.owner}</td>
+                <td>{issue.created.toDateString()}</td>
+                <td>{issue.effort}</td>
+                <td>{issue.completionDate ? issue.completionDate.toDateString() : ''}</td>
+                <td>{issue.title}</td>
             </tr>
         )
     }
@@ -47,6 +54,22 @@ class IssueAdd extends React.Component {
     }
 }
 
+const issues = [
+    {
+        id: 1, status: 'Open', owner: 'Ravan',
+        created: new Date('2016-08-15'), effort: 5,
+        completionDate: undefined,
+        title: 'Error in console when clicking Add',
+    },
+    {
+        id: 2, status: 'Assigned', owner: 'Eddie',
+        created: new Date('2016-08-16'), effort: 14,
+        completionDate: new Date('2016-08-30'),
+        title: 'Missing bottom border on panel',
+    }
+
+]
+
 class IssueList extends React.Component {
     render() {
         return (
@@ -54,7 +77,7 @@ class IssueList extends React.Component {
                 <h1>Issue Tracker</h1>
                 <IssueFilter />
                 <hr />
-                <IssueTable />
+                <IssueTable issues={issues} />
                 <hr />
                 <IssueAdd />
             </div>
