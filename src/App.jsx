0771d524f022e3f6dc87af04f4ea66b8bfd9b8b5
david <dsmith@securescan.com>
2022-11-28 16:27:18 -0500

    state

--- src/App.jsx
+++ src/App.jsx
@@ -71,6 +71,25 @@ const issues = [
 ]
 
 class IssueList extends React.Component {
+    constructor() {
+        super();
+        this.state = { issues: issues };
+        setTimeout(this.createTestIssue.bind(this), 2000);
+    }
+
+    createIssue(newIssue) {
+        const newIssues = this.state.issues.slice();
+        newIssue.id = this.state.issues.length + 1;
+        newIssues.push(newIssue);
+        this.setState({ issues: newIssues });
+    }
+
+    createTestIssue() {
+        this.createIssue({
+            status: 'New', owner: 'Pieta', created: new Date(),
+            title: 'Completion date should be optional',
+        });
+    }
     render() {
         return (
             <div>
