256e58d7ccce5ce3d42310c885915dc4954a075d
david <dsmith@securescan.com>
2022-12-05 12:53:04 -0500

    Nested Routes

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -23,6 +23,14 @@ const issuesDB = [
     created: new Date('2019-01-15'),
     due: undefined,
     title: 'Error in console when clicking Add',
+    description: 'Steps to recreate the problem:'
+      + '\n1. Refresh the browser.'
+      + '\n2. Select "New" in the filter'
+      + '\n3. Refresh the browser again. Note the warning in the console:'
+      + '\n Warning: Hash history cannot PUSH the same path; a new entry'
+      + '\n will not be added to the history stack'
+      + '\n4. Click on Add.'
+      + '\n5. There is an error in console, and add doesn\'t work.',
   },
   {
     id: 2,
@@ -32,15 +40,17 @@ const issuesDB = [
     created: new Date('2019-01-16'),
     due: new Date('2019-02-01'),
     title: 'Missing bottom border on panel',
+    description: 'There needs to be a border in the bottom in the panel'
+      + ' that appears when clicking on Add',
   },
 ];
 
 db.issues.insertMany(issuesDB);
-const count = db.issues.count();
+const count = db.issues.countDocuments();
 print('Inserted', count, 'issues');
 
-db.counters.remove({ _id: 'issues' });
-db.counters.insert({ _id: 'issues', current: count });
+db.counters.deleteOne({ _id: 'issues' });
+db.counters.insertOne({ _id: 'issues', current: count });
 
 db.issues.createIndex({ id: 1 }, { unique: true });
 db.issues.createIndex({ status: 1 });
