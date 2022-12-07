0e9681cda65e0d506667c93f65857eb974c377a5
david <dsmith@securescan.com>
2022-12-07 12:23:02 -0500

    Validation Alerts

--- ui/src/IssueEdit.jsx
+++ ui/src/IssueEdit.jsx
@@ -10,6 +10,7 @@ import {
   ControlLabel,
   ButtonToolbar,
   Button,
+  Alert,
 } from 'react-bootstrap';
 import DateInput from './DateInput.jsx';
 import graphQLFetch from './graphQLFetch.js';
@@ -23,10 +24,12 @@ export default class IssueEdit extends React.Component {
     this.state = {
       issue: {},
       invalidFields: {},
+      showingValidation: false,
     };
     this.onChange = this.onChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.onValidityChange = this.onValidityChange.bind(this);
+    this.dismissValidation = this.dismissValidation.bind(this);
   }
 
   componentDidMount() {
@@ -60,6 +63,7 @@ export default class IssueEdit extends React.Component {
 
   async handleSubmit(e) {
     e.preventDefault();
+    this.showValidation();
     const { issue, invalidFields } = this.state;
     if (Object.keys(invalidFields).length !== 0) return;
 
@@ -97,6 +101,14 @@ export default class IssueEdit extends React.Component {
     this.setState({ issue: data ? data.issue : {}, invalidFields: {} });
   }
 
+  showValidation() {
+    this.setState({ showingValidation: true });
+  }
+
+  dismissValidation() {
+    this.setState({ showingValidation: false });
+  }
+
   render() {
     const { issue: { id } } = this.state;
     const { match: { params: { id: propsId } } } = this.props;
@@ -107,13 +119,13 @@ export default class IssueEdit extends React.Component {
       return null;
     }
 
-    const { invalidFields } = this.state;
+    const { invalidFields, showingValidation } = this.state;
     let validationMessage;
-    if (Object.keys(invalidFields).length !== 0) {
+    if (Object.keys(invalidFields).length !== 0 && showingValidation) {
       validationMessage = (
-        <div className="error">
+        <Alert bsStyle="danger" onDismiss={this.dismissValidation}>
           Please correct invalid fields before submitting.
-        </div>
+        </Alert>
       );
     }
 
@@ -240,8 +252,10 @@ export default class IssueEdit extends React.Component {
                 </ButtonToolbar>
               </Col>
             </FormGroup>
+            <FormGroup>
+              <Col smOffset={3} sm={9}>{validationMessage}</Col>
+            </FormGroup>
           </Form>
-          {validationMessage}
         </Panel.Body>
         <Panel.Footer>
           <Link to={`/edit/${id - 1}`}>Prev</Link>
