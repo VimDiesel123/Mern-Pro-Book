0217d80f4483a6ba04c00bb43371c68e8477713a
david <dsmith@securescan.com>
2022-12-09 10:53:42 -0500

    Issue Counts API

--- api/issue.js
+++ api/issue.js
@@ -16,6 +16,39 @@ async function list(_, { status, effortMin, effortMax }) {
   return issues;
 }
 
+async function counts(_, { status, effortMin, effortMax }) {
+  const db = getDb();
+  const filter = {};
+  if (status) filter.status = status;
+
+  if (effortMin !== undefined || effortMax !== undefined) {
+    filter.effort = {};
+    if (effortMin !== undefined) filter.effort.$gte = effortMin;
+    if (effortMax !== undefined) filter.effort.$lte = effortMax;
+  }
+
+  const results = await db.collection('issues').aggregate([
+    { $match: filter },
+    {
+      $group: {
+        _id: { owner: '$owner', status: '$status' },
+        count: { $sum: 1 },
+      },
+    },
+  ]).toArray();
+
+  const stats = {};
+  results.forEach((result) => {
+    // eslint-disable-next-line no-underscore-dangle
+    const { owner, status: statusKey } = result._id;
+    if (!stats[owner]) stats[owner] = { owner };
+    stats[owner][statusKey] = result.count;
+  });
+
+  return Object.values(stats);
+}
+
+
 async function get(_, { id }) {
   const db = getDb();
   const issue = await db.collection('issues').findOne({ id });
@@ -76,5 +109,10 @@ async function remove(_, { id }) {
 }
 
 module.exports = {
-  list, add, get, update, delete: remove,
+  list,
+  add,
+  get,
+  update,
+  delete: remove,
+  counts,
 };
