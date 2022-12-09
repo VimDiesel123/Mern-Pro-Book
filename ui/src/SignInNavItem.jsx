eea481fcc392d8fa53a24f15f9dd058fe6188c29
david <dsmith@securescan.com>
2022-12-09 16:22:19 -0500

    Faffing about with google authentication

--- ui/src/SignInNavItem.jsx
+++ ui/src/SignInNavItem.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {
   MenuItem, NavDropdown, Modal, Button, NavItem,
 } from 'react-bootstrap';
+import { GoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
 
 export default class SignInNavItem extends React.Component {
   constructor(props) {
@@ -14,6 +16,19 @@ export default class SignInNavItem extends React.Component {
     this.hideModal = this.hideModal.bind(this);
     this.signOut = this.signOut.bind(this);
     this.signIn = this.signIn.bind(this);
+  }
+
+  componentDidMount() {
+    const clientId = window.ENV.GOOGLE_CLIENT_ID;
+    if (!clientId) return;
+    const initClient = () => {
+      gapi.client.init({
+        clientId,
+        scope: '',
+      });
+    };
+
+    gapi.load('client:auth2', initClient);
   }
 
   signIn() {
@@ -34,6 +49,7 @@ export default class SignInNavItem extends React.Component {
   }
 
   render() {
+    const clientID = window.ENV.GOOGLE_CLIENT_ID;
     const { user } = this.state;
     if (user.signedIn) {
       return (
@@ -53,9 +69,13 @@ export default class SignInNavItem extends React.Component {
             <Modal.Title>Sign in</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Button block bsStyle="primary" onClick={this.signIn}>
-              Sign In
-            </Button>
+            <GoogleLogin
+              clientId={clientID}
+              buttonText="Sign in with Google"
+              onSuccess={this.signIn}
+              cookiePolicy="single_host_origin"
+              isSignedIn
+            />
           </Modal.Body>
           <Modal.Footer>
             <Button bsStyle="link" onClick={this.hideModal}>Cancel</Button>
